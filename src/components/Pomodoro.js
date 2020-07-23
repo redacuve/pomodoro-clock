bfd89753c5dbfdd5be5c4b8881802014e8dad5dc
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-23 17:23:59 -0500

    created react app

---
+++ src/components/Pomodoro.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Pomodoro() {
+  return (
+    <div className="pomodoro">
+      Pomodoro Clock
+    </div>
+  );
+}
+
+export default Pomodoro;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.css';
+import Pomodoro from './components/Pomodoro';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Pomodoro />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
