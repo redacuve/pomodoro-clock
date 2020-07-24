47430190295551b920b5cdd46cd7ca4c86742479
Rey David Cuevas <cuevas.vela@gmail.com>
2020-07-24 11:11:07 -0500

    added test script in the html file and created basic scrtucture of the DOM elementes

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -4,6 +4,24 @@ function Pomodoro() {
   return (
     <div className="pomodoro">
       Pomodoro Clock
+      <div id="break-label">
+        Break Label
+        <button id="break-increment" type="button"></button>
+        <div id="break-length">5</div>
+        <button id="break-decrement" type="button"></button>
+      </div>
+      <div id="session-label">
+        Session Length
+        <button id="session-increment" type="button"></button>
+        <div id="session-length">25</div>
+        <button id="session-decrement" type="button"></button>
+      </div>
+      <div id="timer-label">
+        Session
+      </div>
+      <div id="time-left">25:00</div>
+      <button type="button" id="start_stop"></button>
+      <button type="button" id="reset"></button>
     </div>
   );
 }
